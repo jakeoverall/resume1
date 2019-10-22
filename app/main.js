3a78a1fd159635cce6572f61bd3a6c440381e849
Jake Overall <joverall22@gmail.com>
2019-10-21 20:46:14 -0600

    ready to deploy awesome!!!

---
+++ app/main.js
@@ -0,0 +1,22 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i)
+}
+
+//example only
+var resume = {
+  name: "Jake",
+  bio: "akdfjsladsjfkljasdlkjfas",
+  workHistory: [{
+    company: "codeworks",
+    title: "Instructor",
+    responsibilities: "do things"
+  }, {
+    company: "codeworks",
+    title: "Instructor",
+    responsibilities: "do things"
+  }, {
+    company: "codeworks",
+    title: "Instructor",
+    responsibilities: "do things"
+  }]
+}
